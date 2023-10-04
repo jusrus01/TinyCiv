52bdca3c8b29daad7e2ea1d0424d417c45dafc9c
Justinas Simas <simjustinas@gmail.com>
2023-10-04 13:49:04 +0300

    updated Image class; factory method

--- src/TinyCiv.Client/Code/Images.cs
+++ src/TinyCiv.Client/Code/Images.cs
@@ -11,22 +11,58 @@ namespace TinyCiv.Client.Code
     {
         private static Dictionary<GameObjectType, string> playerRedSources = new()
         {
-            { GameObjectType.Warrior, "/Assets/warriorRed.png" }
+            { GameObjectType.Warrior, "/Assets/warriorRed.png" },
+            { GameObjectType.Colonist, "/Assets/colonistRed.png" },
+            { GameObjectType.Cavalry, "/Assets/cavalryRed.png" },
+            { GameObjectType.City, "/Assets/cityRed.png" },
+            { GameObjectType.Farm, "/Assets/farmRed.png" },
+            { GameObjectType.Mine, "/Assets/mineRed.png" },
+            { GameObjectType.Blacksmith, "/Assets/blacksmithRed.png" },
+            { GameObjectType.Market, "/Assets/marketRed.png" },
+            { GameObjectType.Bank, "/Assets/bankRed.png" },
+            { GameObjectType.Port, "/Assets/portRed.png" },
         };
 
         private static Dictionary<GameObjectType, string> playerGreenSources = new()
         {
-            { GameObjectType.Warrior, "/Assets/warriorGreen.png" }
+            { GameObjectType.Warrior, "/Assets/warriorGreen.png" },
+            { GameObjectType.Colonist, "/Assets/colonistGreen.png" },
+            { GameObjectType.Cavalry, "/Assets/cavalryGreen.png" },
+            { GameObjectType.City, "/Assets/cityGreen.png" },
+            { GameObjectType.Farm, "/Assets/farmGreen.png" },
+            { GameObjectType.Mine, "/Assets/mineGreen.png" },
+            { GameObjectType.Blacksmith, "/Assets/blacksmithGreen.png" },
+            { GameObjectType.Market, "/Assets/marketGreen.png" },
+            { GameObjectType.Bank, "/Assets/bankGreen.png" },
+            { GameObjectType.Port, "/Assets/portGreen.png" },
         };
 
         private static Dictionary<GameObjectType, string> playerYellowSources = new()
         {
-            { GameObjectType.Warrior, "/Assets/warriorYellow.png" }
+            { GameObjectType.Warrior, "/Assets/warriorYellow.png" },
+            { GameObjectType.Colonist, "/Assets/colonistYellow.png" },
+            { GameObjectType.Cavalry, "/Assets/cavalryYellow.png" },
+            { GameObjectType.City, "/Assets/cityYellow.png" },
+            { GameObjectType.Farm, "/Assets/farmYellow.png" },
+            { GameObjectType.Mine, "/Assets/mineYellow.png" },
+            { GameObjectType.Blacksmith, "/Assets/blacksmithYellow.png" },
+            { GameObjectType.Market, "/Assets/marketYellow.png" },
+            { GameObjectType.Bank, "/Assets/bankYellow.png" },
+            { GameObjectType.Port, "/Assets/portYellow.png" },
         };
 
         private static Dictionary<GameObjectType, string> playerPurpleSources = new()
         {
-            { GameObjectType.Warrior, "/Assets/warriorPurple.png" }
+            { GameObjectType.Warrior, "/Assets/warriorPurple.png" },
+            { GameObjectType.Colonist, "/Assets/colonistPurple.png" },
+            { GameObjectType.Cavalry, "/Assets/cavalryPurple.png" },
+            { GameObjectType.City, "/Assets/cityPurple.png" },
+            { GameObjectType.Farm, "/Assets/farmPurple.png" },
+            { GameObjectType.Mine, "/Assets/minePurple.png" },
+            { GameObjectType.Blacksmith, "/Assets/blacksmithPurple.png" },
+            { GameObjectType.Market, "/Assets/marketPurple.png" },
+            { GameObjectType.Bank, "/Assets/bankPurple.png" },
+            { GameObjectType.Port, "/Assets/portPurple.png" },
         };
 
         private static ImageSource LoadImage(string filePath)
