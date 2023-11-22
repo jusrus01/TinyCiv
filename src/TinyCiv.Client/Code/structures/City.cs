5a8b7687a23aa4f1eebe7cccb3b63bca033513f7
Randwarf <matas.vaitkevicius@gmail.com>
2023-11-22 12:41:00 +0200

    area highlight for city range

--- src/TinyCiv.Client/Code/structures/City.cs
+++ src/TinyCiv.Client/Code/structures/City.cs
@@ -1,8 +1,16 @@
 ﻿using TinyCiv.Shared.Game;
+using System.Collections.Generic;
 
 namespace TinyCiv.Client.Code.Structures;
 
 public class City : Structure
 {
+    public List<GameObject> HighlightedArea = new List<GameObject>();
+
     public City(GameObject go) : base(go) { }
+
+    public void RemoveHighlight()
+    {
+        HighlightedArea.ForEach(x => x.RemoveEffects());
+    }
 }
